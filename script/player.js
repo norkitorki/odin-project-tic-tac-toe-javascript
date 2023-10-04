85d08746b099c60611e3d3cfc46e2a681990c9a8
norkitorki <norki@tuta.io>
2023-10-04 13:45:58 +0200

    Define minmax()

--- script/player.js
+++ script/player.js
@@ -5,7 +5,7 @@ const Player = (name, marker, color, parent = document.body, options = {}) => {
         obj         = { name, marker: marker[0], color };
   
   if (marker.length > 1 ) console.warn(`${name}'s marker has been truncated to the first character`);
-  if (options.computer) obj.randomPlacement = randomPlacement;
+  if (options.computer) Object.assign(obj, { randomPlacement, minMax });
 
   _updateName(false);
   colorPicker.value = color;
@@ -15,7 +15,7 @@ const Player = (name, marker, color, parent = document.body, options = {}) => {
   parent.appendChild(template);
 
   function _nameFormat() {
-    return `${obj.name} (${obj.marker})`
+    return `${obj.name} (${obj.marker})`;
   }
 
   function _updateName(promptUser = true) {
@@ -35,6 +35,33 @@ const Player = (name, marker, color, parent = document.body, options = {}) => {
     return emptyFields[index];
   }
 
+  function minMax(board, player, currentPlayer) {
+    let result = board.gameOver(currentPlayer);
+    if (result) {
+      return typeof result === 'object' ? (currentPlayer === this ? 10 : -10) : 0;
+    }
+
+    let scores = [], moves = [], mark;
+    board.emptyFields().forEach(pos => {
+      mark = board.toArray()[pos[0]][pos[1]];
+      board.newGameState(pos, currentPlayer.marker);
+      scores.push(this.minMax(board, player, currentPlayer === player ? this : player) || 0);
+      moves.push(pos);
+      board.newGameState(pos, mark);
+    })
+    
+    let targetScore = scores[0], index = 0;
+    scores.forEach((score, i) => {
+      if (currentPlayer === this ? score > targetScore : score < targetScore) {
+        targetScore = score;
+        index = i;
+      }
+    })
+
+    this.nextMove = moves[index];
+    return scores[index];
+  }
+  
   return obj;
 };
 
